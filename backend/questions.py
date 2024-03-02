fd1e037e8686bd8ff81ccc08d95bd9b51c1b2e64
Etienne Sagon <ejbs1@st-andrews.ac.uk>
2024-03-02 20:10:36 +0000

    more realistic numbers

--- backend/questions.py
+++ backend/questions.py
@@ -1,27 +1,5 @@
 import json
 
-wellbeing = 100
-bank_account = 0
-
-#income
-salary = 0
-returns = 0
-
-#expenses
-transports = 0
-rent = 0
-food = 150
-tax = 0
-savings = 0
-extras = 0
-pension = 0
-investment = 0
-purchase = 0
-
-#bank account
-amount_saved = 0
-amount_invested = 0
-
 initial_variables = {
     "age": 10,
     "wellbeing": 100,
@@ -80,45 +58,45 @@ questions = {
         {"source": 4, "option_id": 9, "option_label": "Rent & bills", "target": 5},
         {"source": 4, "option_id": 10, "option_label": "Transport", "target": 6},
 
-        {"source": 5, "option_id": 11, "option_label": "Basic (£550)", "target": 4, "variables": ["rent"], "amount": [550]},
-        {"source": 5, "option_id": 12, "option_label": "Intermediate (£625)", "target": 4, "variables": ["rent"], "amount": [625]},
-        {"source": 5, "option_id": 13, "option_label": "Fancy (£800)", "target": 4, "variables": ["rent"], "amount": [800]},
+        {"source": 5, "option_id": 11, "option_label": "Basic (£650)", "target": 4, "variables": ["rent", "wellbeing"], "amount": [650, -3]},
+        {"source": 5, "option_id": 12, "option_label": "Intermediate (£800)", "target": 4, "variables": ["rent", "wellbeing"], "amount": [800,1]},
+        {"source": 5, "option_id": 13, "option_label": "Fancy (1100)", "target": 4, "variables": ["rent", "wellbeing"], "amount": [1100,3]},
 
-        {"source": 6, "option_id": 14, "option_label": "Cycle (Initial bike cost £200) [worst wellbeing wise]", "target": 3, "variables": ["transport", "purchase"], "amount": [0, 200]},
-        {"source": 6, "option_id": 15, "option_label": "Public transport (monthly ticket cost of £40 and no maintenance)", "target": 3, "variables": ["transport", "purchase"], "amount": [40, 0]},
-        {"source": 6, "option_id": 16, "option_label": "Buy Car (large initial cost £8k, maintenance £15 and insurance £35)", "target": 3, "variables": ["transport", "purchase"], "amount": [50, 8000]},
-        {"source": 6, "option_id": 17, "option_label": "Lease Car (monthly cost £380, maintenance £12 and insurance £34)", "target": 3, "variables": ["transport", "purchase"], "amount": [426, 0]},
+        {"source": 6, "option_id": 14, "option_label": "Cycle (Initial bike cost £200) [worst wellbeing wise]", "target": 3, "variables": ["transport", "purchase", "wellbeing"], "amount": [0, 200,-3]},
+        {"source": 6, "option_id": 15, "option_label": "Public transport (monthly ticket cost of £50 and no maintenance)", "target": 3, "variables": ["transport", "purchase", "wellbeing"], "amount": [50, 0,-1]},
+        {"source": 6, "option_id": 16, "option_label": "Buy Car (large initial cost £8k, maintenance £30 and insurance 50)", "target": 3, "variables": ["transport", "purchase", "wellbeing"], "amount": [80, 8000,3]},
+        {"source": 6, "option_id": 17, "option_label": "Lease Car (monthly cost £380, maintenance £30 and insurance 50)", "target": 3, "variables": ["transport", "purchase", "wellbeing"], "amount": [450, 0,3]},
 
         {"source": 7, "option_id": 18, "option_label": "Savings", "target": 8},
         {"source": 7, "option_id": 19, "option_label": "Investments", "target": 9},
         {"source": 7, "option_id": 20, "option_label": "Pension", "target": 10},
 
         {"source": 8, "option_id": 21, "option_label": "£0", "target": 7, "variables": ["savings"], "amount": [0]},
-        {"source": 8, "option_id": 22, "option_label": "£50", "target": 7, "variables": ["savings"], "amount": [50]},
-        {"source": 8, "option_id": 23, "option_label": "£100", "target": 7, "variables": ["savings"], "amount": [100]},
+        {"source": 8, "option_id": 22, "option_label": "£150", "target": 7, "variables": ["savings"], "amount": [50]},
+        {"source": 8, "option_id": 23, "option_label": "£300", "target": 7, "variables": ["savings"], "amount": [100]},
 
         {"source": 10, "option_id": 24, "option_label": "Defined contribution pension", "target": 7, "variables": ["pension"], "amount": [0]},
-        {"source": 10, "option_id": 25, "option_label": "Defined benefit pension", "target": 7, "variables": ["pension"], "amount": [50]},
-        {"source": 10, "option_id": 26, "option_label": "State pension", "target": 7, "variables": ["pension"], "amount": [100]},
+        {"source": 10, "option_id": 25, "option_label": "Defined benefit pension", "target": 7, "variables": ["pension"], "amount": [150]},
+        {"source": 10, "option_id": 26, "option_label": "State pension", "target": 7, "variables": ["pension"], "amount": [300]},
 
         {"source": 11, "option_id": 27, "option_label": "Holiday", "target": 12},
         {"source": 11, "option_id": 28, "option_label": "Hobbies", "target": 13},
         {"source": 11, "option_id": 29, "option_label": "Going out (i.e. restaurants, cinema)", "target": 14},
 
-        {"source": 12, "option_id": 30, "option_label": "No holiday", "target": 11, "variables": ["extras"], "amount": [0]},
-        {"source": 12, "option_id": 31, "option_label": "Small holiday (£300/year)", "target": 11, "variables": ["extras"], "amount": [25]},
-        {"source": 12, "option_id": 32, "option_label": "Medium holiday (£800/year)", "target": 11, "variables": ["extras"], "amount": [67]},
-        {"source": 12, "option_id": 33, "option_label": "Big holiday (£1200/year)", "target": 11, "variables": ["extras"], "amount": [100]},
+        {"source": 12, "option_id": 30, "option_label": "No holiday", "target": 11, "variables": ["extras", "wellbeing"], "amount": [0,-3]},
+        {"source": 12, "option_id": 31, "option_label": "Small holiday (£400/year)", "target": 11, "variables": ["extras", "wellbeing"], "amount": [34,-1]},
+        {"source": 12, "option_id": 32, "option_label": "Medium holiday (£800/year)", "target": 11, "variables": ["extras", "wellbeing"], "amount": [67,1]},
+        {"source": 12, "option_id": 33, "option_label": "Big holiday (£1800/year)", "target": 11, "variables": ["extras", "wellbeing"], "amount": [150,3]},
 
-        {"source": 13, "option_id": 34, "option_label": "£0", "target": 11, "variables": ["extras"], "amount": [0]},
-        {"source": 13, "option_id": 35, "option_label": "£20", "target": 11, "variables": ["extras"], "amount": [20]},
-        {"source": 13, "option_id": 36, "option_label": "£50", "target": 11, "variables": ["extras"], "amount": [50]},
-        {"source": 13, "option_id": 37, "option_label": "£80", "target": 11, "variables": ["extras"], "amount": [80]},
+        {"source": 13, "option_id": 34, "option_label": "£0", "target": 11, "variables": ["extras","wellbeing"], "amount": [0,-3]},
+        {"source": 13, "option_id": 35, "option_label": "£30", "target": 11, "variables": ["extras", "wellbeing"], "amount": [20,-1]},
+        {"source": 13, "option_id": 36, "option_label": "£60", "target": 11, "variables": ["extras", "wellbeing"], "amount": [50,1]   },
+        {"source": 13, "option_id": 37, "option_label": "£120", "target": 11, "variables": ["extras", "wellbeing"], "amount": [80,3]},
 
-        {"source": 14, "option_id": 38, "option_label": "£15", "target": 11, "variables": ["extras"], "amount": [15]},
-        {"source": 14, "option_id": 39, "option_label": "£30", "target": 11, "variables": ["extras"], "amount": [30]},
-        {"source": 14, "option_id": 40, "option_label": "£50", "target": 11, "variables": ["extras"], "amount": [50]},
-        {"source": 14, "option_id": 41, "option_label": "£100", "target": 11, "variables": ["extras"], "amount": [100]}
+        {"source": 14, "option_id": 38, "option_label": "£30", "target": 11, "variables": ["extras", "wellbeing"], "amount": [15,-3]},
+        {"source": 14, "option_id": 39, "option_label": "£75", "target": 11, "variables": ["extras", "wellbeing"], "amount": [30,-1]},
+        {"source": 14, "option_id": 40, "option_label": "£150", "target": 11, "variables": ["extras", "wellbeing"], "amount": [50,1]},
+        {"source": 14, "option_id": 41, "option_label": "£250", "target": 11, "variables": ["extras", "wellbeing"], "amount": [100,3]}
     ]   
 
 }
@@ -143,7 +121,10 @@ def outcome(selected_option, global_vars):
     var_name = selected_option["variables"]
     amount = selected_option["amount"]
 
+    
     global_vars_updated = global_vars
+
+    global_vars_updated["purchase"] = 0
 
     for i in range(len(var_name)):
         if var_name[i] == "salary":
