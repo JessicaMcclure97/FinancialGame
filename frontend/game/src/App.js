fedd4ce3f3977df4fe6058bee3c9bdac9da69fa8
jrm37 <jrm37@st-andrews.ac.uk>
2024-03-02 18:54:16 +0000

    Main page

--- frontend/game/src/App.js
+++ frontend/game/src/App.js
@@ -1,11 +1,107 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
 import HelloWorld from './helloWorls'
+
+//Layout 
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
+// React95 
+import { GlobalStyle, ThemeProvider } from '@react95/core'
+import { createGlobalStyle } from 'styled-components'
+
+//Components 
+import TaskBar from './components/Taskbar'
+import QuestionPannel from './components/QuestionPannel';
+import OptionsPannel from './components/OptionsPannel';
+import AboutPannel from './components/AboutPannel';
+import { background, backgroundColor } from '@xstyled/styled-components';
+
+const CustomGlobalStyle = createGlobalStyle`
+  body {
+    user-select: none;
+
+    a {
+      color: ${(props) => props.theme.colors.black};
+    }
+    ul a {
+      text-decoration: none;
+    }
+  }
+`
+
+
+//HomePage 
 function App() {
+  const [currentQuestion, setQuestion] = useState({id: 0, text:"This is a test question"});
+  const [options, setOptions] = useState([]);
+  const [chosenOption, setChosenOption] = useState(null)
+  const [state, setState] = useState({
+    age: 10,
+    wellbeing: 100,
+    bank_account: 0,
+    salary: 0,
+    returns: 0,
+    transports: 0,
+    rent: 0,
+    food: 150,
+    tax: 0,
+    savings: 0,
+    extras: 0,
+    pension: 0,
+    investment: 0,
+    purchase: 0
+  })
+
+  /*
+  API to get question everytime question is chosen 
+  */
+  useEffect(() => {
+    const fetchQuestion = async () => {
+      try {
+        const response = await axios.get('http://localhost:8000/getQuestion',{
+          currentQuestion: currentQuestion.id,
+          chosenOption: {chosenOption},
+          state: {state}, 
+        }); 
+        //Await response for next Question
+        const data = await response.json();
+        setQuestion(data.question); // Update state with the fetched question
+        setOptions(data.options);
+        setState(data.state);
+      } catch (error) {
+        console.error('Error fetching question:', error);
+        // Handle errors appropriately, e.g., display an error message to the user
+      }
+    };
+
+    fetchQuestion();
+  }, [chosenOption]);
+
+  const optionChosen = (option) =>{
+    setChosenOption(option)
+  }
+
   return (
     <div className="App">
-      <HelloWorld />
+       <ThemeProvider
+        breakpoints={['xxxl', 'xxl', 'xl', 'lg', 'md', 'sm', 'xs', 'xxs']}
+       >
+        <GlobalStyle />
+        <CustomGlobalStyle />
+        <h1> Game Title </h1>
+        <QuestionPannel question={currentQuestion}/>
+        <OptionsPannel options={options} chosen={(e) => optionChosen(e)} />
+        <AboutPannel state={state}/>
+        <TaskBar />
+        </ThemeProvider>
+
+     
     </div>
+
   );
 }
 
