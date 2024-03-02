b1d8c6965a3f9d889adeeba4f9e6045d3196071b
Etienne Sagon <ejbs1@st-andrews.ac.uk>
2024-03-02 22:22:18 +0000

    Merge

--- backend/server/myapi/urls.py
+++ backend/server/myapi/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('hello-world/', views.hello_world, name='hello_world'),
+    path('getQuestion/', views.get_Question, name='get_question'),
+
 ]
--- backend/server/myapi/views.py
+++ backend/server/myapi/views.py
@@ -8,3 +8,9 @@ from rest_framework.response import Response
 @api_view(['GET'])
 def hello_world(request):
     return Response({'message': 'Hello, world!'})
+
+@api_view(['GET'])
+def get_question(request):
+    currentQuestion = request.param.currentQuestion 
+    
+    return Response({'message': 'Hello, world!'})
