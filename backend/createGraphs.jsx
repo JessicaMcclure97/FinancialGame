a0d4302f27728f1828af1ad003fb89d7c8b966d2
Zoe G <89026437+zoegemmell@users.noreply.github.com>
2024-03-02 19:46:58 +0000

    Create createGraphs.jsx
    Doing react graphs instead

---
+++ backend/createGraphs.jsx
@@ -0,0 +1,44 @@
+<Plot
+      data={[
+        {
+          x: [1, 2, 3],
+          y: [2, 6, 3],
+          type: 'scatter',
+          mode: 'lines+markers',
+          marker: {color: 'red'},
+        },
+        {type: 'bar', x: [1, 2, 3], y: [2, 5, 3]},
+      ]}
+      layout={ {width: 320, height: 240, title: 'A Fancy Plot'} } />
+
+const IncomeVsExpenses = (props) =>{
+  var data = [
+    {
+         x: [1, 2, 3, 4], // months
+         y: [0, 2, 3, 5], // income = salary + returns
+         type: 'scatter',
+         name: 'Income',
+         line: {color: '#4daf4a'} 
+       },
+       {
+         x: [1, 2, 3, 4], // months
+         y: [3, 5, 1, 7], // expenses = transport + rent + food + tax + savings + extras + pension + investment + purchase
+         type: 'scatter',
+         name: 'Expenses',
+         line: {color: '##e41a1c'},
+         marker: { symbol: 'x', size: 10, line: {width: 2, color: '#e41a1c'} }
+       }
+  ];
+}
+
+const BankBalance = (props) =>{
+  var data = [
+    {
+         x: [1, 2, 3, 4], // months
+         y: [0, 2, 3, 5], // income - expenses
+         type: 'scatter',
+         name: 'Balance',
+         line: {color: '#377eb8'} 
+       }
+  ];
+}
